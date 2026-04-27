9fd38d87749dd632762d36f94a081d0ebf560bd3
Nhan Nguyen <nhannt.itech@gmail.com>
2026-04-27 08:40:48 +0700

    update: hide schema on swagger

--- config/initializers/rswag_ui.rb
+++ config/initializers/rswag_ui.rb
@@ -3,5 +3,6 @@
 if defined?(Rswag::Ui)
   Rswag::Ui.configure do |c|
     c.openapi_endpoint '/api-docs/v1/openapi.yaml', 'ShortLink API V1'
+    c.config_object['defaultModelsExpandDepth'] = -1
   end
 end
